e7b231dd5999e634566b527eba39bc6e2ef44e61
Antonio Román <kyradiscord@gmail.com>
2021-01-27 12:47:29 +0100

    chore: add codecov, update cov requirements

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,9 @@ module.exports = {
 	coverageReporters: ['html', 'text', 'clover'],
 	coverageThreshold: {
 		global: {
-			branches: 80,
-			functions: 80,
-			lines: 80,
-			statements: 80,
+			branches: 70,
+			lines: 70,
+			statements: 70,
 		},
 	},
 	roots: ['<rootDir>packages/'],
