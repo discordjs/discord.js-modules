25afe7b8f51ddb20a3fa82e479c7c9f78dacd917
Vlad Frangu <kingdgrizzle@gmail.com>
2021-01-28 14:54:08 +0200

    chore: suggested change
    Co-authored-by: Jan <66554238+vaporox@users.noreply.github.com>

--- packages/rest/src/lib/errors/DiscordAPIError.ts
+++ packages/rest/src/lib/errors/DiscordAPIError.ts
@@ -20,7 +20,7 @@ function isErrorGroupWrapper(error: any): error is DiscordErrorGroupWrapper {
 }
 
 function isErrorResponse(error: any): error is DiscordErrorFieldInformation {
-	return typeof Reflect.get(error, 'message') === 'string';
+	return typeof error.message === 'string';
 }
 
 /**
