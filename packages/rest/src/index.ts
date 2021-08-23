d4ec4a529808e2d9e1b5b3699a085c0ca08f48a0
Jeroen Claassens <support@favware.tech>
2021-08-23 13:56:37 +0200

    fix(Rest): use reference type for DOM
    We had someone in Sapphire running into an issue here because they
    didn't have "DOM" in their tsconfig lib, nor did they have
    "skipLibCheck" set to true. While those 2 options are viable
    end-user solutions, it would be better for the lib to actually
    just function without the need for that.
    
    This can be achieved with a simple triple slash directive, which unlike
    the lib config in tsconfig.json does get copied over to the build
    output `index.d.ts`

--- packages/rest/src/index.ts
+++ packages/rest/src/index.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line spaced-comment
+/// <reference lib="dom" />
+
 export * from './lib/CDN';
 export * from './lib/errors/DiscordAPIError';
 export * from './lib/errors/HTTPError';
