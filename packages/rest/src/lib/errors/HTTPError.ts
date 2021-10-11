2bb5441ad765002d51a1a676ff8d23253505e026
ckohen <chaikohen@gmail.com>
2021-10-10 23:04:45 -0700

    feat(Errors): show data sent when an error occurs
    Ported from https://github.com/discordjs/discord.js/pull/5701
    
    Co-authored-by: Vlad Frangu <kingdgrizzle@gmail.com>

--- packages/rest/src/lib/errors/HTTPError.ts
+++ packages/rest/src/lib/errors/HTTPError.ts
@@ -1,13 +1,18 @@
+import type { InternalRequest } from '../RequestManager';
+import type { RequestBody } from './DiscordAPIError';
+
 /**
  * Represents a HTTP error
  */
 export class HTTPError extends Error {
+	public requestBody: RequestBody;
 	/**
 	 * @param message The error message
 	 * @param name The name of the error
 	 * @param status The status code of the response
 	 * @param method The method of the request that erred
 	 * @param url The url of the request that erred
+	 * @param bodyData The unparsed data for the request that erred
 	 */
 	public constructor(
 		message: string,
@@ -15,7 +20,10 @@ export class HTTPError extends Error {
 		public status: number,
 		public method: string,
 		public url: string,
+		bodyData: Pick<InternalRequest, 'attachments' | 'body'>,
 	) {
 		super(message);
+
+		this.requestBody = { attachments: bodyData.attachments, json: bodyData.body };
 	}
 }
