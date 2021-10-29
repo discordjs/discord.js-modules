b73cc060daa701de71815a824ebaccdc9ebf2859
ckohen <chaikohen@gmail.com>
2021-10-29 06:00:35 -0700

    feat(Rest): improve global rate limit and invalid request tracking (#51)
    Co-authored-by: Vlad Frangu <kingdgrizzle@gmail.com>
    Co-authored-by: Antonio Román <kyradiscord@gmail.com>
    Co-authored-by: Noel <icrawltogo@gmail.com>

--- packages/rest/__tests__/RequestHandler.test.ts
+++ packages/rest/__tests__/RequestHandler.test.ts
@@ -1,13 +1,21 @@
 import nock from 'nock';
-import { DefaultRestOptions, DiscordAPIError, HTTPError, REST } from '../src';
+import { DefaultRestOptions, DiscordAPIError, HTTPError, REST, RESTEvents } from '../src';
 
-const api = new REST({ timeout: 2000 }).setToken('A-Very-Fake-Token');
+const api = new REST({ timeout: 2000, offset: 5 }).setToken('A-Very-Fake-Token');
 const invalidAuthApi = new REST({ timeout: 2000 }).setToken('Definitely-Not-A-Fake-Token');
 
 let resetAfter = 0;
+let sublimitResetAfter = 0;
+let retryAfter = 0;
+let sublimitRequests = 0;
+let sublimitHits = 0;
 let serverOutage = true;
 let unexpected429 = true;
 let unexpected429cf = true;
+const sublimitIntervals = {
+	reset: null,
+	retry: null,
+};
 
 nock(`${DefaultRestOptions.api}/v${DefaultRestOptions.version}`)
 	.persist()
@@ -63,6 +71,75 @@ nock(`${DefaultRestOptions.api}/v${DefaultRestOptions.version}`)
 	})
 	.get('/regularRequest')
 	.reply(204, { test: true })
+	.patch('/channels/:id', (body) => ['name', 'topic'].some((key) => Reflect.has(body, key)))
+	.reply(function handler(): nock.ReplyFnResult {
+		sublimitHits += 1;
+		sublimitRequests += 1;
+		const response = 2 - sublimitHits >= 0 && 10 - sublimitRequests >= 0 ? 204 : 429;
+		startSublimitIntervals();
+		if (response === 204) {
+			return [
+				204,
+				undefined,
+				{
+					'x-ratelimit-limit': '10',
+					'x-ratelimit-remaining': `${10 - sublimitRequests}`,
+					'x-ratelimit-reset-after': ((sublimitResetAfter - Date.now()) / 1000).toString(),
+					via: '1.1 google',
+				},
+			];
+		}
+		return [
+			429,
+			{
+				limit: '10',
+				remaining: `${10 - sublimitRequests}`,
+				resetAfter: (sublimitResetAfter / 1000).toString(),
+				retryAfter: ((retryAfter - Date.now()) / 1000).toString(),
+			},
+			{
+				'x-ratelimit-limit': '10',
+				'x-ratelimit-remaining': `${10 - sublimitRequests}`,
+				'x-ratelimit-reset-after': ((sublimitResetAfter - Date.now()) / 1000).toString(),
+				'retry-after': ((retryAfter - Date.now()) / 1000).toString(),
+				via: '1.1 google',
+			},
+		];
+	})
+	.patch('/channels/:id', (body) => ['name', 'topic'].every((key) => !Reflect.has(body, key)))
+	.reply(function handler(): nock.ReplyFnResult {
+		sublimitRequests += 1;
+		const response = 10 - sublimitRequests >= 0 ? 204 : 429;
+		startSublimitIntervals();
+		if (response === 204) {
+			return [
+				204,
+				undefined,
+				{
+					'x-ratelimit-limit': '10',
+					'x-ratelimit-remaining': `${10 - sublimitRequests}`,
+					'x-ratelimit-reset-after': ((sublimitResetAfter - Date.now()) / 1000).toString(),
+					via: '1.1 google',
+				},
+			];
+		}
+		return [
+			429,
+			{
+				limit: '10',
+				remaining: `${10 - sublimitRequests}`,
+				resetAfter: (sublimitResetAfter / 1000).toString(),
+				retryAfter: ((sublimitResetAfter - Date.now()) / 1000).toString(),
+			},
+			{
+				'x-ratelimit-limit': '10',
+				'x-ratelimit-remaining': `${10 - sublimitRequests}`,
+				'x-ratelimit-reset-after': ((sublimitResetAfter - Date.now()) / 1000).toString(),
+				'retry-after': ((sublimitResetAfter - Date.now()) / 1000).toString(),
+				via: '1.1 google',
+			},
+		];
+	})
 	.get('/unexpected')
 	.times(2)
 	.reply(function handler(): nock.ReplyFnResult {
@@ -117,6 +194,44 @@ nock(`${DefaultRestOptions.api}/v${DefaultRestOptions.version}`)
 	.get('/malformedRequest')
 	.reply(601);
 
+// This is tested first to ensure the count remains accurate
+test('Significant Invalid Requests', async () => {
+	const invalidListener = jest.fn();
+	const invalidListener2 = jest.fn();
+	api.on(RESTEvents.InvalidRequestWarning, invalidListener);
+	// Ensure listeners on REST do not get double added
+	api.on(RESTEvents.InvalidRequestWarning, invalidListener2);
+	api.off(RESTEvents.InvalidRequestWarning, invalidListener2);
+	const [a, b, c, d, e] = [
+		api.get('/badRequest'),
+		api.get('/badRequest'),
+		api.get('/badRequest'),
+		api.get('/badRequest'),
+		api.get('/badRequest'),
+	];
+	await expect(a).rejects.toThrowError('Missing Permissions');
+	await expect(b).rejects.toThrowError('Missing Permissions');
+	await expect(c).rejects.toThrowError('Missing Permissions');
+	await expect(d).rejects.toThrowError('Missing Permissions');
+	await expect(e).rejects.toThrowError('Missing Permissions');
+	expect(invalidListener).toHaveBeenCalledTimes(0);
+	api.requestManager.options.invalidRequestWarningInterval = 2;
+	const [f, g, h, i, j] = [
+		api.get('/badRequest'),
+		api.get('/badRequest'),
+		api.get('/badRequest'),
+		api.get('/badRequest'),
+		api.get('/badRequest'),
+	];
+	await expect(f).rejects.toThrowError('Missing Permissions');
+	await expect(g).rejects.toThrowError('Missing Permissions');
+	await expect(h).rejects.toThrowError('Missing Permissions');
+	await expect(i).rejects.toThrowError('Missing Permissions');
+	await expect(j).rejects.toThrowError('Missing Permissions');
+	expect(invalidListener).toHaveBeenCalledTimes(3);
+	api.off(RESTEvents.InvalidRequestWarning, invalidListener);
+});
+
 test('Handle standard rate limits', async () => {
 	const [a, b, c] = [api.get('/standard'), api.get('/standard'), api.get('/standard')];
 
@@ -135,6 +250,40 @@ test('Handle global rate limits', async () => {
 	expect(await api.get('/triggerGlobal')).toStrictEqual({ global: true });
 	expect(await api.get('/regularRequest')).toStrictEqual({ test: true });
 	expect(Date.now()).toBeGreaterThanOrEqual(earlier + 100);
+});
+
+test('Handle sublimits', async () => {
+	const sublimit = { body: { name: 'newname' } };
+	const noSublimit = { body: { bitrate: 40000 } };
+	// Return the current time on these results as their response does not indicate anything
+	// Queue all requests, don't wait, to allow retroactive check
+	const [aP, bP, cP, dP, eP] = [
+		api.patch('/channels/:id', sublimit).then(() => Date.now()),
+		api.patch('/channels/:id', sublimit).then(() => Date.now()),
+		api.patch('/channels/:id', sublimit).then(() => Date.now()), // Limit hits
+		api.patch('/channels/:id', noSublimit).then(() => Date.now()), // Ensure normal request passes
+		api.patch('/channels/:id', sublimit).then(() => Date.now()), // For retroactive check
+	];
+
+	const [a, b, c, d] = await Promise.all([aP, bP, cP, dP]);
+
+	const [f, g] = await Promise.all([
+		api.patch('/channels/:id', sublimit).then(() => Date.now()),
+		api.patch('/channels/:id', noSublimit).then(() => Date.now()),
+	]); // For additional sublimited checks
+	const e = await eP;
+
+	expect(a).toBeLessThan(b);
+	expect(b).toBeLessThan(c);
+	expect(d).toBeLessThan(c);
+	expect(c).toBeLessThan(e);
+	expect(d).toBeLessThan(e);
+	expect(e).toBeLessThan(f);
+	expect(e).toBeLessThan(g);
+	expect(g).toBeLessThan(f);
+
+	clearInterval(sublimitIntervals.reset);
+	clearInterval(sublimitIntervals.retry);
 });
 
 test('Handle unexpected 429', async () => {
@@ -179,3 +328,22 @@ test('Unauthorized', async () => {
 test('malformedRequest', async () => {
 	expect(await api.get('/malformedRequest')).toBe(null);
 });
+
+function startSublimitIntervals() {
+	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+	if (!sublimitIntervals.reset) {
+		sublimitResetAfter = Date.now() + 250;
+		sublimitIntervals.reset = setInterval(() => {
+			sublimitRequests = 0;
+			sublimitResetAfter = Date.now() + 250;
+		}, 250);
+	}
+	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+	if (!sublimitIntervals.retry) {
+		retryAfter = Date.now() + 1000;
+		sublimitIntervals.retry = setInterval(() => {
+			sublimitHits = 0;
+			retryAfter = Date.now() + 1000;
+		}, 1000);
+	}
+}
